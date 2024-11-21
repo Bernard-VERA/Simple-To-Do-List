45c8b172317629a1d31145d63aa61b36167ea6c3
Bernard-VERA <bvmars@hotmail.com>
2024-11-21 01:15:14 +0100

    Création du script et mise en forme

---
+++ script.js
@@ -0,0 +1,29 @@
+function addTask() {
+    // Récupération du texte de l'input. Trim supprime les espaces vides
+    const input = document.getElementById("todo-input");
+    const todoText = input.value.trim();
+    // Si aucun texte on arrète la fonction
+    if (todoText === "") return;
+
+        // Création d'une liste qui reprend le texte récupéré précédemment
+        const todoList = document.getElementById("todo-list");
+        const li = document.createElement("li");
+        li.textContent = todoText;
+
+            // Création d'un bouton " Supprimer". Au clic, on supprime la tâche.
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Supprimer";
+            deleteButton.classList.add("delete-button");
+            deleteButton.onclick = function() {
+                todoList.removeChild(li)
+            };
+
+            //Association du bouton "Supprimer" avec la liste à supprimer 
+            li.appendChild(deleteButton);
+
+        //Affichage de la liste créée
+        todoList.appendChild(li);
+
+    // Une fois la tâche créée, on réinitialise le champ de texte
+    input.value = "";
+}
