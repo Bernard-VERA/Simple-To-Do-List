dc750cec899ce510887126100b8622b86626df99
Bernard-VERA <bvmars@hotmail.com>
2024-11-23 11:06:30 +0100

    Ajout du local storage pour stocker les tâches

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', loadTasks);
+
 function addTask() {
     // Récupération du texte de l'input. Trim supprime les espaces vides
     const input = document.getElementById("todo-input");
@@ -18,15 +20,73 @@ function addTask() {
             deleteButton.textContent = "Supprimer";
             deleteButton.classList.add("delete-button");
             deleteButton.onclick = function() {
-                todoList.removeChild(li)
+                todoList.removeChild(li);
+                saveTasks();
             };
 
             //Association de la span et du bouton "Supprimer" avec la liste à supprimer 
             li.appendChild(textSpan);
             li.appendChild(deleteButton);
+            li.addEventListener('click', () => { 
+                li.classList.toggle('completed'); 
+                saveTasks();
+            });
+
         //Affichage de la liste créée
         todoList.appendChild(li);
 
+        // Sauvegarde des tâches dans le localStorage 
+        saveTasks();
+
     // Une fois la tâche créée, on réinitialise le champ de texte
     input.value = "";
+}
+
+function saveTasks() { 
+    const todoList = document.getElementById("todo-list"); 
+    const todos = []; 
+
+    todoList.querySelectorAll("li").forEach(todoItem => { 
+        todos.push({ 
+            text: todoItem.querySelector("span").textContent, 
+            completed: todoItem.classList.contains("completed") 
+        }); 
+    });
+    // Sauvegarde des tâches dans le local storage sous forme de JSON
+    localStorage.setItem("todos", JSON.stringify(todos)); 
+    console.log(todos);
+} 
+
+function loadTasks() {
+    // Récupération des tâches dans le local storage (JSON)
+    const todos = JSON.parse(localStorage.getItem("todos")) || [];
+     
+    todos.forEach(todo => {
+        const todoList = document.getElementById("todo-list"); 
+        const li = document.createElement("li"); 
+        const textSpan = document.createElement("span"); 
+        textSpan.textContent = todo.text; 
+        textSpan.classList.add("todo-item"); 
+         
+        if (todo.completed) { 
+            li.classList.add("completed"); 
+        } 
+        
+        const deleteButton = document.createElement("button"); 
+        deleteButton.textContent = "Supprimer"; 
+        deleteButton.classList.add("delete-button"); 
+        deleteButton.onclick = function() { 
+            todoList.removeChild(li); 
+            saveTasks(); 
+        }; 
+        
+        li.appendChild(textSpan); 
+        li.appendChild(deleteButton); 
+        li.addEventListener('click', () => { 
+            li.classList.toggle('completed'); 
+            saveTasks();
+        }); 
+        
+        todoList.appendChild(li); 
+    }); 
 }
